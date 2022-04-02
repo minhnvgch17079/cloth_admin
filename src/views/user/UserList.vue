360d7e50ffa2f27d9829689123051cf367b01ee8
Sotatek-MinhNguyen4 <minh.nguyen4@sotatek.com>
2022-04-02 17:56:41 +0700

    save

--- src/views/user/UserList.vue
+++ src/views/user/UserList.vue
@@ -7,7 +7,7 @@
         <b-form-select v-model="roleSearch" :options="listGroup"></b-form-select>
       </b-col>
       <b-col>
-        <b-input :placeholder="`Username`" v-model="usernameSearch"></b-input>
+        <b-input :placeholder="`Email`" type="email" v-model="emailSearch"></b-input>
       </b-col>
       <b-col>
         <b-input :placeholder="`Full name`" v-model="fullNameSearch"></b-input>
@@ -16,24 +16,9 @@
         <b-input :placeholder="`Phone Number`" v-model="phoneNumberSearch"></b-input>
       </b-col>
       <b-col>
-        <b-input :placeholder="`Email`" type="email" v-model="emailSearch"></b-input>
-      </b-col>
-      <b-col>
         <b-btn class="mr-3" variant="outline-info" @click="getUser()">Search</b-btn>
-        <b-btn variant="outline-success" v-b-modal.modal-1>Add User</b-btn>
       </b-col>
     </b-row>
-    <b-modal centered id="modal-1" title="Add New User" @ok="addUser()">
-      <b-row>
-        <b-input class="ml-3 mr-3 mb-3" v-model="usernameAdd" :placeholder="`Username`"></b-input>
-      </b-row>
-      <b-row>
-        <b-input class="ml-3 mr-3 mb-3" v-model="passwordAdd" :placeholder="`Password`"></b-input>
-      </b-row>
-      <b-row>
-        <b-form-select class="ml-3 mr-3 mb-3" v-model="roleAdd" :options="optionsRole"></b-form-select>
-      </b-row>
-    </b-modal>
     <br>
     <br>
     <b-row>
@@ -61,13 +46,6 @@
         :per-page="perPage"
         :current-page="currentPage"
       >
-        <template v-slot:cell(is_active)="row">
-          <b-badge variant="success" v-if="row.item.is_active === 1">ACTIVE</b-badge>
-          <b-badge variant="danger" v-if="row.item.is_active !== 1">DISABLED</b-badge>
-        </template>
-        <template v-slot:cell(setFaculty)="row">
-          <b-btn variant="outline-primary" size="sm" @click="addFaculty(row.item)">Set Faculty</b-btn>
-        </template>
         <template v-slot:cell(manage)="row">
           <b-btn variant="outline-danger" @click="disabledAccount(row.item.id)">
             <feather-icon icon="TrashIcon" svgClasses="h-4 w-4"/>
@@ -124,29 +102,21 @@ export default {
       currentPage: 1,
       fieldsDataUsers: [
         {key: 'id', label: 'Id', sortable: true},
-        {key: 'username', label: 'Username', sortable: true},
-        {key: 'setFaculty', label: 'Set Faculty', sortable: true},
-        {key: 'is_active', label: 'Status', sortable: true},
-        {key: 'full_name', label: 'Full name', sortable: true},
-        {key: 'group_name', label: 'Role', sortable: true},
-        {key: 'phone_number', label: 'Phone Number', sortable: true},
         {key: 'email', label: 'Email', sortable: true},
-        {key: 'created', label: 'Created', sortable: true},
-        {key: 'modified', label: 'Modified', sortable: true},
-        {key: 'modified_by', label: 'Modified by', sortable: true},
+        {key: 'role', label: 'Chức Năng', sortable: true},
+        {key: 'full_name', label: 'Họ Tên', sortable: true},
+        {key: 'address', label: 'Địa Chỉ', sortable: true},
+        {key: 'status', label: 'Trạng Thái', sortable: true},
         {key: 'manage', label: 'Manage'}
       ],
       dataUsers: [],
       filter: null,
 
       roleSearch: null,
-      usernameSearch: '',
       fullNameSearch: '',
       phoneNumberSearch: '',
       emailSearch: '',
 
-      usernameAdd: '',
-      passwordAdd: '',
       optionsRole: [
         { value: null, text: 'Please select role' },
         { value: 1, text: 'Admin' },
@@ -155,7 +125,6 @@ export default {
         { value: 4, text: 'Marketing Manager' },
         { value: 5, text: 'Guest' },
       ],
-      roleAdd: null,
       userIdAddFaculty: null,
       listGroup: [],
     }
@@ -174,8 +143,8 @@ export default {
       .then((res) => {
         this.listGroup = Object.values(res.data.data).map(e => {
           return {
-            value: e.id,
-            text: e.name
+            value: e,
+            text: e
           }
         })
         this.listGroup.push({
@@ -194,41 +163,22 @@ export default {
     },
     getUser () {
       let data = {
-        username: this.usernameSearch,
         full_name: this.fullNameSearch,
         phone_number: this.phoneNumberSearch,
         email: this.emailSearch,
         group_id: this.roleSearch
       }
       this.dataUsers = []
-      Service.getListUser(data).then(res => {
-        if (res.data.success) {
+      axios.get(`${BACKEND_BASE_URL}/user/user-list`, {'params': data})
+        .then(res => {
           this.dataUsers = res.data.data
-          return commonHelper.showMessage(res.data.message, 'success')
-        }
-        commonHelper.showMessage(res.data.message || 'There something error. Please try again', 'warning')
-      }).catch(() => {
-        commonHelper.showMessage('There something error. Please try again', 'warning')
-      });
+          return commonHelper.showMessage(SUCCESS_COMMON, 'success')
+        })
+        .catch(() => {
+          commonHelper.showMessage(ERROR_COMMON, 'warning')
+        })
     },
 
-    addUser () {
-      let data = {
-        username: this.usernameAdd,
-        password: this.passwordAdd,
-        group_id: this.roleAdd
-      }
-      Service.addUser(data).then(res => {
-        if (res.data.success) {
-          this.usernameSearch = this.usernameAdd
-          this.getUser()
-          return commonHelper.showMessage(res.data.message, 'success')
-        }
-        commonHelper.showMessage(res.data.message || 'There something error. Please try again', 'warning')
-      }).catch(() => {
-        commonHelper.showMessage('There something error. Please try again', 'warning')
-      });
-    },
     disabledAccount (idAccount) {
       if (!confirm('Are you sure want to block this account?')) return 1
 
